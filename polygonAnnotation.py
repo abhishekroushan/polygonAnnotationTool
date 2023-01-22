48d259fda5ebd601902d4a3e8f5305aeb2bcf10b
abhishekroushan <abhishek.roushan12@gmail.com>
2023-01-21 22:36:06 -0800

    add tkinter canvas and image to demo annotation tool in action

--- polygonAnnotation.py
+++ polygonAnnotation.py
@@ -7,6 +7,8 @@ annotation, segmenting, machine learning etc.
 Author: Abhishek Roushan abhishek.roushan12@gmail.com
 """
 
+import tkinter as tk
+from PIL import ImageTk, Image
 import json
 from json import JSONEncoder
 
@@ -15,7 +17,7 @@ class Point2d(object):
         self.x = x
         self.y = y
     def data(self):
-        return x, y
+        return self.x, self.y
     def resetToOrigin(self):
         self.x = 0
         self.y = 0
@@ -35,6 +37,19 @@ class Polygon(object):
         self.points.append(point2d)
     def clear(self):
         self.points = []
+    def isValid(self):
+        if len(self.points)<3: return False
+        return True
+    def lastPoint(self):
+        if len(self.points)==0: 
+            print("no points in Polygon")
+            return None
+        return self.points[-1]
+    def firstPoint(self):
+        if len(self.points)==0: 
+            print("no points in Polygon")
+            return None
+        return self.points[0]
 
 class TextWriter(object):
     def __init__(self, txtFileName:str):
@@ -48,6 +63,66 @@ class TextWriter(object):
         with open(self.file, "a+") as txtFile:
             for i in range(0, len(polygons)):
                 self.writePolygonToFile(polygons[i], txtFile)
+
+
+class Canvas:
+    def __init__(self):
+        self.C = None
+        self.polys = []
+        self.currPoly = Polygon([])
+    def polygons(self):
+        return self.polys
+    def create(self):
+        top = tk.Tk()
+        self.C = tk.Canvas(top, bg="blue", height=400, width=550)
+        filename = ImageTk.PhotoImage(Image.open("trees.png")) # using PIL
+        image = self.C.create_image(20, 20, anchor="nw", image=filename)
+        self.C.bind("<Button-1>", self.drawPolyLine)
+        completePolyBtn = tk.Button(top, text='Complete Polygon', width=20,
+                     height=5, bd='5', command=self.completePoly)
+        completePolyBtn.place(x=10, y=300)
+
+        writeAllPolyBtn = tk.Button(top, text='Write Polygons', width=20,
+                                    height=5, bd='5', command=self.writePolygons)
+        writeAllPolyBtn.place(x=250, y=300)
+        
+        self.C.pack()
+        top.mainloop()
+    def drawPolyLine(self, event):
+        x,y = self.getCoordinate(event)
+        lp = self.currPoly.lastPoint()
+        if lp is not None:
+            # add x,y to self.currPoly and create a line from last point
+            lpx, lpy = lp.data()
+            self.C.create_line(lpx, lpy, x, y, width=5)
+        # create currPoly with new point from x,y
+        self.currPoly.addPoint(Point2d(x,y))
+    def completePoly(self):
+        fp = self.currPoly.firstPoint()
+        if fp is None:
+            print("can't complete current Polygon. try adding more points")
+        else:
+            if not self.currPoly.isValid():
+                print("can't complete current Polygon. try adding more points")
+                return
+            fpx, fpy = fp.data()
+            lp = self.currPoly.lastPoint()
+            lpx, lpy = lp.data() # directly query data since self.currPoly is valid
+            self.C.create_line(lpx, lpy, fpx, fpy, width=5)
+            self.polys.append(self.currPoly)
+            self.currPoly.clear()
+            print("current Polygon completed, and added to Canvas polygons")
+    def getCoordinate(self, event):
+        x = event.x
+        y = event.y
+        print("clicked at x=", x, ", y=", y)
+        return x,y
+    def writePolygons(self):
+        polys = self.polygons()
+        txtFileName = "./data.txt"
+        writer = TextWriter(txtFileName)
+        writer.writePolygonsToFile(polys)
+        print("Polygons written to file:", txtFileName)
 
 
 
